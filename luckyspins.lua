88fea543ce6247b18d645e4bb499dfa3418feb27
ericbemlindo <105563783+ericbemlindo@users.noreply.github.com>
2026-02-24 13:56:35 -0300

    Create luckyspins.lua

---
+++ luckyspins.lua
@@ -0,0 +1,36 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+
+local ClaimReward = ReplicatedStorage
+    :WaitForChild("Packages")
+    :WaitForChild("_Index")
+    :WaitForChild("sleitnick_knit@1.7.0")
+    :WaitForChild("knit")
+    :WaitForChild("Services")
+    :WaitForChild("ChallengeService")
+    :WaitForChild("RF")
+    :WaitForChild("ClaimReward")
+
+
+local startId = 1
+local endId = 1000 
+
+print("Iniciando a coleta de recompensas...")
+
+for i = startId, endId do
+    local args = { i }
+
+    
+    local success, result = pcall(function()
+        return ClaimReward:InvokeServer(unpack(args))
+    end)
+
+    if success then
+        print("Recompensa " .. i .. " processada. Resposta: ", result)
+    else
+        warn("Erro ao processar ID " .. i .. ": " .. tostring(result))
+    end
+
+    
+    task.wait(0.5) 
+end
